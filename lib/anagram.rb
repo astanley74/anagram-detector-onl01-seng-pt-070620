1a451891ebe5628759ed5dfa17f5b7b6a984f0fa
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-24 21:09:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.each do |word|
+      new_word = word.chars.sort.join
+        if new_word.count > 2
+      
+    end
+  end
+end
